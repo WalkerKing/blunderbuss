9d9d3350763323d436a7fdd4a9aacb79541e2d6d
Walker <walker.study@foxmail.com>
2020-08-17 16:33:39 +0800

    Add animate.css

--- 50-vuex/src/router/index.js
+++ 50-vuex/src/router/index.js
@@ -1,16 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import home from '@/views/home'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: home
-    }
-  ]
-})
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+  ],
+});
